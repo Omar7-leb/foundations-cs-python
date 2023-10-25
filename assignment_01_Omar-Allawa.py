2fd87d23ce4f47810e968c1b8024640de196e2ce
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-10-25 14:47:13 +0300

    divisors and reverse added!

--- assignment_01_Omar-Allawa.py
+++ assignment_01_Omar-Allawa.py
@@ -13,3 +13,27 @@ while True:
     result = factorial(user_input)
     if result is not None:
         print("Factorial of " + f"{user_input} is {result}")
+        
+#2------------------------------------------------------------
+def divisors(n):
+    list = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            list.append(i)
+    return list
+while True:
+    user_input = int(input("Enter a positive number"))
+    result = divisors(user_input)
+    print(f"Divisors of {user_input}: {result}")
+    
+
+#3--------------------------------------------------------
+def reverse(word):
+    reverse = ""
+    for i in range(len(word)-1, -1, -1):
+        reverse += word[i]
+    return reverse
+while True:   
+    user_input = input("Give a word to reverse it : ")
+    result = reverse(user_input)
+    print("the reverse of " + user_input + " is : " + result) 
