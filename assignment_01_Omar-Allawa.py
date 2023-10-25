6b3fa67c121a477b4f3a282a2df07858482c3fc1
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-10-25 14:49:47 +0300

    even numbers and password functions added!

--- assignment_01_Omar-Allawa.py
+++ assignment_01_Omar-Allawa.py
@@ -37,3 +37,47 @@ while True:
     user_input = input("Give a word to reverse it : ")
     result = reverse(user_input)
     print("the reverse of " + user_input + " is : " + result) 
+    
+#4----------------------------------------------------------
+def Evennumber(input_list):
+    even_numbers = []
+    for i in input_list:
+        if i % 2 == 0:
+            even_numbers.append(i)
+    return even_numbers
+
+list_length = int(input('Give the length of the list: '))
+user_list = []
+for i in range(list_length):
+    user_input = int(input("Enter a number: "))
+    user_list.append(user_input)
+result = Evennumber(user_list)
+print(user_list)
+print(result)
+
+#5--------------------------------------------------------
+def Password(password):
+    length_condition = len(password) >= 8
+    uppercase_condition = False
+    lowercase_condition = False
+    digit_condition = False
+    special_condition = False
+    
+    for char in password:
+        if char.isupper():
+            uppercase_condition = True
+        elif char.islower():
+            lowercase_condition = True
+        elif char.isdigit():
+            digit_condition = True
+        elif char in '#?!$':
+            special_condition = True
+
+    if length_condition and uppercase_condition and lowercase_condition and digit_condition and special_condition:
+        return "Strong Password"
+    else:
+        return "Weak Password"
+while True:
+    password = input("Give me a password: ")
+    result = Password(password)
+    print(result)
