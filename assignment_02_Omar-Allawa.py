74ba5ffe1629b5a6dd21b34a2c0d979dcb5226cd
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-10-26 18:32:46 +0300

    function findmax added!

--- assignment_02_Omar-Allawa.py
+++ assignment_02_Omar-Allawa.py
@@ -6,3 +6,11 @@ def CountDigits(number):
         number //= 10
         CountDigits(number)
     return counter
+
+#2--------------------------------
+def FindMax(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        max_rest = FindMax(lst[1:])
+        return max_rest if max_rest > lst[0] else lst[0]
