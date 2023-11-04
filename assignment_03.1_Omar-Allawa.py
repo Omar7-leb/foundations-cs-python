ba246b948f0919be31655d21b8df83a2dcaf194f
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-04 16:26:40 +0200

    add,common,delet functions are added!

--- assignment_03.1_Omar-Allawa.py
+++ assignment_03.1_Omar-Allawa.py
@@ -16,3 +16,29 @@ def GetStudentByMajor(student_data, major):
         if student["Major"] == major:
             commonMajor.append(student["Name"])
     return commonMajor
+
+#4----------------------------------------------------------------
+def addStudent(student_data, id, name, age, major, GPA):
+    new_student = {"ID": id, "Name": name, "Age": age, "Major": major, "GPA": GPA}
+    student_data.append(new_student)
+    print("Student added successfully")
+
+#5----------------------------------------------------------------
+def CommonMajor(student_data, student_data_2):
+    common_Students = set()
+    for student in student_data:
+        for student2 in student_data_2:
+            if student['Major'] == student2["Major"]:
+                common_Students.add(student["Major"])
+    return common_Students
+
+#6-----------------------------------------------------------------
+def delete_student(students, id):
+    updated_students = [student for student in students if student['ID'] != id]
+    if len(updated_students) == len(students):
+        print("Student not found!")
+    else:
+        print("Student with ID:", id, "was deleted successfully.")
+    return updated_students
+
+
