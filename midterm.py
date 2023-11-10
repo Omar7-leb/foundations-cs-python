24f63b095bc4f78bad596c313308921cb240e57d
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-10 20:44:08 +0200

    open browser function added!

--- midterm.py
+++ midterm.py
@@ -1,3 +1,11 @@
+def OpenTab(browser,title,url):
+    new_tab = {
+        'Title': title,
+        'URL': url
+    }
+    browser.append(new_tab)
+    print("Tab with title: " + title + " and url: " + url +" has been opened successfully") 
+
 def main():
     browser = []
     print("Hello user , choose an option from the following options:")
@@ -5,6 +13,12 @@ def main():
       print("1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Clear All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit")
       try:
           user_input = int(input("Please choose an option : "))
+          if user_input == 1:
+              title = input("Please enter the title: ")
+              url = input("Please enter the url: ")
+              OpenTab(browser, title, url)
+          else:
+              print("Invalid input. Please choose an option from 1 to 9.")
       except ValueError:
           print("Invalid input. Please enter a number.")
     
