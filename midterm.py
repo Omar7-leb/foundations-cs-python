5db5935b758255a6cd8c614e61f8a02c69d96ee3
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-12 11:08:52 +0200

    handle user input in open nested tab function!

--- midterm.py
+++ midterm.py
@@ -95,8 +95,16 @@ def open_nested_tab(browser, parent_index):
         parent_tab = browser[-1]
 
     nested_tabs = []
-    num_tabs = int(input("Enter the number of nested tabs you want to create: "))
-    
+    try:
+      num_tabs = int(input("Enter the number of nested tabs you want to create: "))
+      
+    except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+            
+    if num_tabs <= 0:
+       print("Number of nested tabs must be a positive integer.")
+       return
+
     for i in range(num_tabs):
         title = input(f"Enter the title for nested tab {i + 1}: ")
         url = input(f"Enter the URL for nested tab {i + 1}: ")
