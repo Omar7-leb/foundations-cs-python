bc30947fabe4cb5992c0cdca8509c6b4542c0cda
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-22 12:59:53 +0200

    fix on class task

--- assignment_04_Omar-Allawa.py
+++ assignment_04_Omar-Allawa.py
@@ -1,27 +1,32 @@
 class Task:
-     def __init__(self, task_id, description, priority , completed):
-         self.task_id = task_id
+    task_id = 0
+    def __init__(self, description, priority , completed):
+         Task.task_id += 1
          self.description = description
          self.priority = priority
          self.completed = completed
+         
+    def get_task_id(self):
+        return self.task_id
           
-     def get_descriptions(self):
+    def get_descriptions(self):
            return  self.description
         
-     def set_descriptions(self, description):
+    def set_descriptions(self, description):
          self.description = description
       
-     def get_priority(self):
+    def get_priority(self):
            return self.priority
       
-     def set_priority(self, priority):
+    def set_priority(self, priority):
          self.priority = priority
         
-     def get_completed(self):
+    def get_completed(self):
           return  self.completed
           
-     def set_completed(self, completed):
-          self.completed = completed
+    def set_completed(self, completed):
+          self.completed = True
+          
       
            
       
