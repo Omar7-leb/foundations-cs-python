21d85991b65b1e0813122a93b00106688f1e72ff
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-22 15:05:19 +0200

    add enqueue function

--- assignment_04_Omar-Allawa.py
+++ assignment_04_Omar-Allawa.py
@@ -1,7 +1,8 @@
 class Task:
     task_id = 0
-    def __init__(self, description, priority , completed):
+    def __init__(self, description, priority , completed = False):
          Task.task_id += 1
+         self.task_id = Task.task_id
          self.description = description
          self.priority = priority
          self.completed = completed
@@ -9,10 +10,10 @@ class Task:
     def get_task_id(self):
         return self.task_id
           
-    def get_descriptions(self):
+    def get_description(self):
            return  self.description
         
-    def set_descriptions(self, description):
+    def set_description(self, description):
          self.description = description
       
     def get_priority(self):
@@ -35,6 +36,7 @@ class Node:
         
 class PriorityQueue:
     def __init__(self):
+        Task.task_id = 0
         self.header = None
         self.size = 0       
         
@@ -43,12 +45,29 @@ class PriorityQueue:
     
     def displayQueue(self):
         current = self.header
-        
-        while(current.next != None):
-            print(f"The id of task :{current.task.get_id()}, Priority :{current.task.get_priority()}, Description :{current.task.get_description()}")
+
+        while current is not None:
+            print(f"The id of task: {current.task.get_task_id()}, Priority: {current.task.get_priority()}, Description: {current.task.get_description()}")
             current = current.next
             
+    def enqueue(self, task):
+        node = Node(task)
         
+        if self.isEmpty() or task.get_priority() > self.header.task.get_priority():
+            node.next = self.header
+            self.header = node
+            
+        else:
+            current = self.header
+            
+            while current.next != None and task.get_priority() <=current.next.task.get_priority():
+               current = current.next
+               
+            node.next = current.next
+            current.next = node 
+
+           
+            
         
         
       
