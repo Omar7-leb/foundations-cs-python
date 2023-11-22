18417a37e33f131fb6dcef8b44104cf03aa93fd0
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-22 13:05:35 +0200

    add node class

--- assignment_04_Omar-Allawa.py
+++ assignment_04_Omar-Allawa.py
@@ -28,6 +28,9 @@ class Task:
           self.completed = True
           
       
-           
+class Node:
+    def __init__(self, task):
+        self.task = task
+        self.next = None           
       
             
