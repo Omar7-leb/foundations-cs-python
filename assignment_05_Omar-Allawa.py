df8fd900867904a5854fbdbff8f2aeda43190a8f
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-30 22:16:02 +0200

    add user input!

---
+++ assignment_05_Omar-Allawa.py
@@ -0,0 +1,7 @@
+def main():
+    print ("Hello , there are a list of choices:")
+    user_input = int(input(" 1. Add Family Member\n 2. Display Sorted Birthdays\n 3. Find Relationship\n 4. Visualize Family Tree\n 5. Count Same First Names\n 6. Exit\n please enter you choice here : "))
+    
+if __name__ == "__main__":
+    main()
+   
