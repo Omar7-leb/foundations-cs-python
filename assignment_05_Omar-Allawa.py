9e65b477b439f12c87fa08f111a9b664f07938cb
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-11-30 22:27:21 +0200

    add node class

--- assignment_05_Omar-Allawa.py
+++ assignment_05_Omar-Allawa.py
@@ -1,3 +1,11 @@
+class Node:
+    def __init__(self , name , family_name , birthdate):
+        self.right = None
+        self.left = None
+        self.name = name
+        self.family_name = family_name
+        self.birthdate = birthdate
+
 def main():
     print ("Hello , there are a list of choices:")
     user_input = int(input(" 1. Add Family Member\n 2. Display Sorted Birthdays\n 3. Find Relationship\n 4. Visualize Family Tree\n 5. Count Same First Names\n 6. Exit\n please enter you choice here : "))
