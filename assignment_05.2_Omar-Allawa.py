a507ee25302b307b5091f2063eab0852eafc6913
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-12-02 17:54:01 +0200

    add a user input

---
+++ assignment_05.2_Omar-Allawa.py
@@ -0,0 +1,9 @@
+
+
+def main() :
+    print("Hello, there are a list of choices:")
+    while True :
+        user_input = int (input(" 1. Add a user to the platform.\n 2. Remove a user from the platform.\n 3. Send a friend request to another user.\n 4. Remove a friend from your list.\n 5. View your list of friends.\n 6. View the list of users on the platform.\n 7. Exit\n Please enter your choice here: "))
+    
+if __name__ == "__main__":
+    main()
